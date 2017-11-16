2ce045c9914749f1fe8a107f0f0c860beb0d90ff
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-16 09:02:52 +0100

    Create 19_10_2017.js

---
+++ motions/19_10_2017.js
@@ -0,0 +1,40 @@
+var dmotion = {
+  dhtml : `<ul>
+                        <li>Debate topic for the 19th of October: <br> "This house believes that leaving dictators in charge of countries is better than to oust these dictators."
+                        <br><br>
+                        <b>Room 430</b><br><br>
+                        PROPOSITION (FOR):<br>
+                        <b>1.</b> Ella B (S3)
+                        <b>2.</b> Sofia (S3)
+                        <b>3.</b> Olivia (s4)
+                        <b>4.</b> Michael (S4)
+                        <br><br>
+                        OPPOSITION (AGAINST):<br>
+                        <b>1.</b> Anya (S3)
+                        <b>2.</b> Nea (S3)
+                        <b>3.</b> Tobias (S4)
+                        <b>4.</b> Kertu (S4)
+                        <br><br>
+                        <b>Room 432</b><br><br>
+                        PROPOSITION (FOR):<br>
+                        <b>1.</b> Julie (S3)
+                        <b>2.</b> Tiago (s3)
+                        <b>3.</b> Lian (S4)
+                        <b>4.</b> Laura (S4)
+                        <br><br>
+                        OPPOSITION (AGAINST):<br>
+                        <b>1.</b> Amalia (S3)
+                        <b>2.</b> Daniel (S3)
+                        <b>3.</b> Lorenzo (S4)
+                        <b>4.</b> Fabian (S4)
+                          <br><br>
+                        Substitutes between both rooms:<br>
+                        1. Adam (s3)
+                        2. Thomas (s3)
+                        3. Vassia (S3)
+                        4. Alma (S4)
+                        5. Giulia (s4)
+                        6. Jonas (S4)</li><br><br>
+                        
+                    </ul>`
+};
