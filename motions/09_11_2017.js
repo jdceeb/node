da0efec761d8221b4cb0c1de9affc9765125893e
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-16 08:59:52 +0100

    Create 09_11_2017.js

---
+++ motions/09_11_2017.js
@@ -0,0 +1,35 @@
+exports.run = {
+ p : `<li>Debate topic for the 9th of November: <br> <span class="motion"> "This house believes violence in TV, film and gaming negatively affects society"</span>
+                    <br><br>
+                    <b>Room 430</b><br><br>
+                    PROPOSITION (FOR):<br>
+                    <b>1.</b> William (S3)
+                    <b>2.</b> Amanda (S3)
+                    <b>3.</b> Laura (s4)
+                    <b>4.</b> Martynas (S4)<br><br>
+                    OPPOSITION (AGAINST):<br>
+                    <b>1.</b> Ashenafy (S3)
+                    <b>2.</b> Eleanor (S3)
+                    <b>3.</b> Michael (S4)
+                    <b>4.</b> Samuel (S4)
+                    <br><br>
+                    <b>Room 425</b><br><br>
+                    PROPOSITION (FOR):<br>
+                    <b>1.</b> Alexander (S3)
+                    <b>2.</b> Matteo (s3)
+                    <b>3.</b> Tobias (S4)
+                    <b>4.</b> Kristaps (S4)<br><br>
+                    OPPOSITION (AGAINST):<br>
+                    <b>1.</b> Emma BM (S3)
+                    <b>2.</b> Clara (S3)
+                    <b>3.</b> Jonas (S4)
+                    <b>4.</b> Kertu (S4)
+                      <br><br>
+                    Substitutes between both rooms:<br>
+                    1. Ella (s3)
+                    2. Sofia (s3)
+                    3. Anya (S3)
+                    4. Megan (S4)
+                    5. Lorenzo (s4)
+                    6. Fabian (S4)</li><br><br>`
+};
