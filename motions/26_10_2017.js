0da55ddf5877cc1bff3bb71aa1ff572549be730a
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-16 09:05:01 +0100

    Create 26_10_2017.js

---
+++ motions/26_10_2017.js
@@ -0,0 +1,38 @@
+var dmotion = {
+ dhtml : `<ul>
+                    <li>Debate topic for the 26th of October: <br> <span class="motion"> "This house believes that that the legal drinking age across the EU should be lowered to 16"</span>
+                    <br><br>
+                    <b>Room 430</b><br><br>
+                    PROPOSITION (FOR):<br>
+                    <b>1.</b> Adam (S3)
+                    <b>2.</b> Thomas (S3)
+                    <b>3.</b> Jonas (s4)
+                    <b>4.</b> Kertu (S4)<br><br>
+                    OPPOSITION (AGAINST):<br>
+                    <b>1.</b> Vassia (S3)
+                    <b>2.</b> Emma B (S3)
+                    <b>3.</b> Alma (S4)
+                    <b>4.</b> Giulia (S4)
+                    <br><br>
+                    <b>Room 432</b><br><br>
+                    PROPOSITION (FOR):<br>
+                    <b>1.</b> Francisco (S3)
+                    <b>2.</b> Kintija (s3)
+                    <b>3.</b> Megan (S4)
+                    <b>4.</b> Lorenzo (S4)<br><br>
+                    OPPOSITION (AGAINST):<br>
+                    <b>1.</b> Roisin (S3)
+                    <b>2.</b> Eleonora (S3)
+                    <b>3.</b> Michael (S4)
+                    <b>4.</b> Samuel (S4)
+                      <br><br>
+                    Substitutes between both rooms:<br>
+                    1. William (s3)
+                    2. Amanda (s3)
+                    3. Ashenafy (S3)
+                    4. Laura (S4)
+                    5. Martynas (s4)
+                    6. Kristaps (S4)</li><br><br>
+
+                </ul>`
+};
