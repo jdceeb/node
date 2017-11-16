31702e58ba8dee05b458ecf7bb3daf2b571297da
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-16 08:57:23 +0100

    Create committee.js

---
+++ committee.js
@@ -0,0 +1,11 @@
+var committee = {
+ dhtml : `<div class="social">
+          <p></p>
+          <ul>
+            <br>
+            <li><a href="https://jdc.js.org/committee/motions/motionsone.pdf" class="index-button">over 500 motions</a></li>
+          <li><a href="https://jdc.js.org/committee/motions/motionstwo.pdf" class="index-button">20 motions</a></li><br><br><br>
+      </ul>
+     </div>
+     <p><a class="button" href="https://jdc.js.org/dev-node-rep">Go Back Here</a></p>`
+};
