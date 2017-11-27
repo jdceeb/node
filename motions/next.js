a47728cbea705e392323ec33284a571db7e88059
KDiaCodes <KDiaCodes@users.noreply.github.com>
2017-11-27 10:25:37 +0100

    Create next.js

---
+++ motions/next.js
@@ -0,0 +1,48 @@
+var dmotion = {
+  dhtml: `<div class="social">
+                <ul>
+                    <li>Debate topic for the 30th of November: <br> <span class="motion"> "This house would replace teachers with robots."</span>
+                    <br><br>
+                    Room 430<br><br>
+                    PROPOSITION (FOR):<br>
+                    1.Francisco (S3)
+                    2. Kintija (S3)
+                    3. Laura (s4)
+                    4. Martynas (S4)
+                    <br><br>
+                    OPPOSITION (AGAINST):<br>
+                    1. William (S3)
+                    2. Amanda (S3)
+                    3. Alma (S4)
+                    4. Giulia (S4)
+                    <br><br>
+                    Substitutes:<br>
+                    1. Emma (s3)
+                    2. Clara (s3)
+                    3. Ella (S3)
+                    4. Michael (S4)
+                    5. Samuel (s4)
+                    <br><br>
+                    Room 425<br><br>
+                    PROPOSITION (FOR):<br>
+                    1. Ashenafy (S3)
+                    2. Eleanor (s3)
+                    3. Oskar (S4)
+                    4. Kristaps (S4)<br><br>
+                    OPPOSITION (AGAINST):<br>
+                    1. Alexander (S3)
+                    2. Matteo (S3)
+                    3. Olivia (S4)
+                    4. Tobias (S4)
+                      <br><br>
+                    Substitutes:<br>
+                    1. Sofia (s3)
+                    2. Anya (s3)
+                    3. Amalia (S3)
+                    4. Jonas (S4)
+                    5. Kertu (s4)</li><br><br>
+                </ul>
+                
+            </div>
+            <p><a class="button" href="https://jdc.js.org/">Go Back Here</a></p>`
+};
